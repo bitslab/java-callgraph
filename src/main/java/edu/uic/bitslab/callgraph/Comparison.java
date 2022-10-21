1695e898726ff3343822f189abc55417e6aa5870
Jesse Coultas <jesse@jessecoultas.com>
2022-10-20 21:36:27 -0500

    Progress on comparison tool for RQ1

--- src/main/java/edu/uic/bitslab/callgraph/Comparison.java
+++ src/main/java/edu/uic/bitslab/callgraph/Comparison.java
@@ -3,7 +3,9 @@ package edu.uic.bitslab.callgraph;
 import gr.gousiosg.javacg.dyn.Pair;
 import gr.gousiosg.javacg.stat.coverage.ColoredNode;
 import gr.gousiosg.javacg.stat.coverage.JacocoCoverage;
+import gr.gousiosg.javacg.stat.coverage.Report;
 import gr.gousiosg.javacg.stat.support.RepoTool;
+import org.apache.commons.cli.*;
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultEdge;
 import org.slf4j.Logger;
@@ -11,13 +13,26 @@ import org.slf4j.LoggerFactory;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
+import java.util.*;
 
 public class Comparison {
+    private static final String PROJECT_NAME = "p";
+    private static final String PROJECT_NAME_LONG = "project";
+    private static final String OUTPUT_FILE_NAME = "o";
+    private static final String OUTPUT_FILE_NAME_LONG = "output";
+    private static final String ARTIFACT_TIMESTAMP = "t";
+    private static final String ARTIFACT_TIMESTAMP_LONG = "timestamp";
+
+
     private static final Logger LOGGER = LoggerFactory.getLogger(Comparison.class);
 
-    public String pruneAndJaCoCo(String jacocoXMLFilename, String prunedGraphSerFile) {
-        StringBuilder s = new StringBuilder();
+    private static String getKey(String entryPoint, String method) {
+        return entryPoint + ":" + method;
+    }
+
+
+    public Map<String, Row> pruneAndJaCoCo(String jacocoXMLFilename, String prunedGraphSerFile) {
+        Map<String, Row> rpt = new HashMap<>();
 
         try {
             Graph<ColoredNode, DefaultEdge> prunedGraph;
@@ -30,7 +45,6 @@ public class Comparison {
                 throw new Exception("pruned graph is null");
             }
 
-            JacocoCoverage jacocoCoverage = new JacocoCoverage(jacocoXMLFilename);
             String entryPoint = prunedGraph
                     .vertexSet()
                     .stream()
@@ -39,23 +53,43 @@ public class Comparison {
                     .findFirst()
                     .orElse(null);
 
-            s.append("entryPoint,method,branchesCovered,branchesMissed,linesCovered,linesMissed,inJaCoCo,nodeColor\n");
-
             for (ColoredNode v : prunedGraph.vertexSet()) {
-                String methodSignature = v.getLabel();
+                String method = v.getLabel();
+                String key = getKey(entryPoint, method);
 
-                s.append("\"").append(entryPoint).append("\",")
-                    .append("\"").append(methodSignature).append("\",")
-                    .append(v.getBranchesCovered()).append(",")
-                    .append(v.getBranchesMissed()).append(",")
-                    .append(v.getLinesCovered()).append(",")
-                    .append(v.getLinesMissed()).append(",")
-                    .append((jacocoCoverage.containsMethod(methodSignature) ? "1" : "0")).append(",")
-                    .append(v.getColor())
-                    .append("\n");
+                Row r = rpt.getOrDefault(key, new Row(entryPoint, method));
+                r.addPruned(v.getColor());
+                rpt.put(key, r);
             }
 
-            return s.toString();
+            JacocoCoverage jacocoCoverage = new JacocoCoverage(jacocoXMLFilename);
+            Map<String, Report.Package.Class.Method> methodCoverage = jacocoCoverage.getMethodCoverage();
+            methodCoverage.forEach(
+                (method, details) -> {
+                    String key = getKey(entryPoint, method);
+                    Row r = rpt.getOrDefault(key, new Row(entryPoint, method));
+                    int branchesCovered = Integer.MIN_VALUE;
+                    int branchedMissed = Integer.MIN_VALUE;
+                    int linesCovered = Integer.MIN_VALUE;
+                    int linesMissed = Integer.MIN_VALUE;
+
+                    for (Report.Package.Class.Method.Counter counter : details.getCounter()) {
+                        if (counter.getType().equals("BRANCH")) {
+                            branchesCovered = counter.getCovered();
+                            branchedMissed = counter.getMissed();
+                        } else if (counter.getType().equals("LINE")) {
+                            linesCovered = counter.getCovered();
+                            linesMissed = counter.getMissed();
+                        }
+                    }
+
+                    r.addJaCoCo(branchesCovered, branchedMissed, linesCovered, linesMissed);
+
+                    rpt.put(key, r);
+                }
+            );
+
+            return rpt;
         } catch (Exception exception) {
             LOGGER.error(exception.getMessage(), exception);
         }
@@ -73,28 +107,64 @@ public class Comparison {
         return null;
     }
 
-    public static void main(String[] args) throws Exception {
-        String project = args[0];
-        String timeStamp;
+    private static Path getLatestResultPath(String project) throws IOException {
+        String resultsDir = "artifacts/results/";
+        String glob = project + "????-??-??T??_??_??.??????";
+        Path latestPath = null;
 
-        if (args.length == 2) {
-            timeStamp = args[1];
-        } else {
-            // get last one in results
-            String resultsDir = "artifacts/results/";
-            String glob = project + "????-??-??T??_??_??.??????";
-            Path latestPath = null;
-
-            for (Path path : Files.newDirectoryStream(Path.of(resultsDir), glob)) {
-                if (latestPath == null || path.compareTo(latestPath) > 0) {
-                    latestPath = path;
-                }
+        for (Path path : Files.newDirectoryStream(Path.of(resultsDir), glob)) {
+            if (latestPath == null || path.compareTo(latestPath) > 0) {
+                latestPath = path;
             }
+        }
+
+        return latestPath;
+    }
+
+
+
+    public static void main(String[] args) throws Exception {
+        String project = null;
+        String timeStamp = null;
+        String outputFile = null;
+
+        /* Setup cmdline argument parsing */
+        CommandLineParser parser = new DefaultParser();
+        Options options = getOptions();
+        CommandLine cmd;
+
+        try {
+            cmd = parser.parse(options, args);
+            if (cmd.hasOption(PROJECT_NAME)) {
+                project = cmd.getOptionValue(PROJECT_NAME);
+            }
+
+            if (cmd.hasOption(ARTIFACT_TIMESTAMP)) {
+                timeStamp = cmd.getOptionValue(ARTIFACT_TIMESTAMP);
+            }
+
+            if (cmd.hasOption(OUTPUT_FILE_NAME)) {
+                outputFile = cmd.getOptionValue(OUTPUT_FILE_NAME);
+            }
+        } catch(ParseException pe) {
+            LOGGER.error("Error parsing command-line arguments: " + pe.getMessage());
+            LOGGER.error("Please, follow the instructions below:");
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("Build comparison for RQ1", options);
+            System.exit(1);
+        }
+
+        if (timeStamp == null) {
+            // get last one in results
+
+            Path latestPath = getLatestResultPath(project);
 
             if (latestPath == null) {
                 LOGGER.error("No result directory found for " + project + ".");
                 System.exit(1);
             }
+
+            assert project != null;
 
             timeStamp = latestPath.getFileName().toString().substring(project.length());
         }
@@ -109,10 +179,109 @@ public class Comparison {
             String jacocoXMLFilename = coverageFile.first;
             String prunedGraphSerFile = coverageFile.first.substring(0, coverageFile.first.length()-4) + "-reachability.ser";
 
-            String result = comparison.pruneAndJaCoCo(jacocoXMLFilename, prunedGraphSerFile);
-            System.out.println(result);
+            Map<String, Row> rpt = comparison.pruneAndJaCoCo(jacocoXMLFilename, prunedGraphSerFile);
 
+
+            String header = String.join(",",
+                            "entryPoint", "method", "nodeColor",
+                            "branchesCovered", "branchesMissed", "linesCovered", "linesMissed",
+                            "inJaCoCo", "inPrunedGraph"
+                    );
+
+
+            if (outputFile == null) {
+                System.out.println(header);
+                rpt.forEach((k, r) -> System.out.println(r));
+            } else {
+                try(FileWriter writer = new FileWriter(outputFile)) {
+                    writer.write(header + "\n");
+
+                    for (Row r : rpt.values()) {
+                        writer.write(r + "\n");
+                    }
+                }
+            }
+        }
+    }
+
+    static class Row {
+        private final String entryPoint;
+        private final String method;
+        private String nodeColor = "";
+        private int branchesCovered = Integer.MIN_VALUE;
+        private int branchesMissed = Integer.MIN_VALUE;
+        private int linesCovered = Integer.MIN_VALUE;
+        private int linesMissed = Integer.MIN_VALUE;
+        private boolean inJaCoCo = false;
+        private boolean inPrunedGraph = false;
+
+        Row(String entryPoint, String method) {
+            this.entryPoint = entryPoint;
+            this.method = method;
         }
 
+        public void addJaCoCo(int branchesCovered, int branchesMissed, int linesCovered, int linesMissed) {
+            this.branchesCovered = branchesCovered;
+            this.branchesMissed = branchesMissed;
+            this.linesCovered = linesCovered;
+            this.linesMissed = linesMissed;
+            this.inJaCoCo = true;
+        }
+
+        public void addPruned(String nodeColor) {
+            this.nodeColor = nodeColor;
+            this.inPrunedGraph = true;
+        }
+
+        @Override
+        public String toString() {
+            return String.join(",",
+                    "\"" + entryPoint + "\"",
+                    "\"" + method + "\"",
+                    "\"" + nodeColor + "\"",
+                    cntToStr(branchesCovered),
+                    cntToStr(branchesMissed),
+                    cntToStr(linesCovered),
+                    cntToStr(linesMissed),
+                    inJaCoCo ? "Y" : "N",
+                    inPrunedGraph ? "Y" : "N"
+            );
+        }
+
+        private String cntToStr(int cnt) {
+            return cnt == Integer.MIN_VALUE ? "UNK" : String.valueOf(cnt);
+        }
     }
+
+    private static Options getOptions() {
+        Options options = new Options();
+        options.addOption(
+                Option.builder(PROJECT_NAME)
+                        .longOpt(PROJECT_NAME_LONG)
+                        .hasArg(true)
+                        .desc("[REQUIRED] specify the project name")
+                        .required(true)
+                        .build());
+
+        options.addOption(
+                Option.builder(OUTPUT_FILE_NAME)
+                        .longOpt(OUTPUT_FILE_NAME_LONG)
+                        .hasArg(true)
+                        .desc("[OPTIONAL] specify the output filename (default to stdout)")
+                        .required(false)
+                        .build());
+
+
+        options.addOption(
+                Option.builder(ARTIFACT_TIMESTAMP)
+                        .longOpt(ARTIFACT_TIMESTAMP_LONG)
+                        .hasArg(true)
+                        .desc("[OPTIONAL] specify the artifact timestamp (defaults to latest run in artifacts)")
+                        .required(false)
+                        .build());
+
+
+        return options;
+    }
+
 }
