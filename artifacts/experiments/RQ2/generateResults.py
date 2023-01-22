ab0189ad26cf7b7ac152ee8207c057597756c1bd
Alekh Meka <alekhmeka@gmail.com>
2023-01-22 14:11:29 -0500

    Updated RQ2

--- artifacts/experiments/RQ2/generateResults.py
+++ artifacts/experiments/RQ2/generateResults.py
@@ -9,7 +9,9 @@ PROJECTS = ["convex", "jflex", "rpki-commons"]
 REPORT_NAME = "artifacts/output/rq2.csv"
 TEX_REPORT_NAME = "artifacts/output/rq2.tex"
 
+PROJECTS = ["convex"]
 ITERATIONS = [10, 50, 500, 1000]
+RAW_NAMES = ["Property", "10", "50", "500", "1000"]
 
 propertyShortNames = {
     "TestSmartListSerializer#canRoundTripSerializableLists": 'list',
@@ -30,7 +32,6 @@ propertyShortNames = {
 def obtain_stats_directories(results_directory: str) -> list[str]:
     directory_tree = [x for x in os.walk(
         results_directory)]  # os.walk returns a tuple with structure (directory, subdirectories, files)
-    print(directory_tree[0])
     return directory_tree[0][1]
 
 
@@ -84,7 +85,6 @@ def obtain_iteration_stats(iteration_directory: str) -> dict[str, tuple]:
     files = [x for x in os.walk(
         iteration_directory)][0][2]
     stats_files = list(filter(lambda stat_file: "reachability-coverage.csv" in stat_file, files))
-    print(stats_files)
     time_files = [f.replace("-reachability-coverage.csv", ".html") for f in stats_files]
     ret = {}
     for file, time_file in zip(stats_files, time_files):
@@ -96,7 +96,6 @@ def obtain_iteration_stats(iteration_directory: str) -> dict[str, tuple]:
 
 
 def generate_project_df(project_ds: dict[int, dict]) -> pd.DataFrame():
-    print(project_ds)
     project_df = pd.DataFrame()
     valid_keys = project_ds[10].keys()  # grab first dict keys for property names
     project_df['Property'] = [key for key in valid_keys]
@@ -104,8 +103,16 @@ def generate_project_df(project_ds: dict[int, dict]) -> pd.DataFrame():
         iteration_property_dict = project_ds[key]
         for vk in valid_keys:
             if vk not in iteration_property_dict:
-                iteration_property_dict[vk] = np.nan
+                iteration_property_dict[vk] = (np.nan, np.nan)
         project_df[key] = [val for val in project_ds[key].values()]
+        # coverage = []
+        # times = []
+        # for k, v in iteration_property_dict.items():
+        #     coverage.append(v[0])
+        #     times.append(v[1])
+        #
+        # project_df[key] = [c for c in coverage]
+        # project_df[key].loc['Time (sec)'] = [t for t in times]
     return project_df
 
 
@@ -124,6 +131,44 @@ def main():
             project_dataset[iteration] = iteration_stats
         final_dataset[project] = generate_project_df(project_ds=project_dataset)
     print(final_dataset)
+
+    with open(TEX_REPORT_NAME, 'w') as tf:
+        df = pd.DataFrame()
+        for project in PROJECTS:
+            final_dataset[project]['_style'] = ''
+            header = dict(zip(['N', 'Property', '10', '50', '100', '500', '1000'], ['', '', '', '', '', '', '']))
+            df = pd.concat([
+                df,
+                pd.DataFrame(header | {'_style': 'HEADER', 'Property': project}, index=[0]),
+                final_dataset[project]
+            ], ignore_index=True)
+
+        bold_rows = df[ df['_style'] == 'BOLD' ].index
+        header_rows = df[ df['_style'] == 'HEADER' ].index
+        latexTable = df \
+            .drop(columns=['_style']) \
+            .style \
+            .hide(axis=0) \
+            .format(precision=2) \
+            .set_properties(subset=pd.IndexSlice[header_rows, :], **{'HEADER': ''}) \
+            .set_properties(subset=pd.IndexSlice[bold_rows, :], **{'textbf': '--rwrap'}) \
+            .to_latex(hrules=False)
+
+        outTable = ''
+
+        # transform to sub headers
+        for line in latexTable.splitlines(keepends=True):
+            s = line.split('&')
+            c = str(len(s))
+
+            possibleCommand = s[0].strip()
+
+            if possibleCommand == '\HEADER':
+                outTable += '\\hline' + "\n" + '\multicolumn{' + c + '}{c}{' + s[1].strip()[7:].strip() + '}' + " \\\\\n" + '\\hline' + "\n"
+            else:
+                outTable += line
+
+        tf.write(outTable)
 
 
 if __name__ == "__main__":
