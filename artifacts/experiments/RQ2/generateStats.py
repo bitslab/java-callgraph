04dd300c065610508de0fc4bfc23462ee78a7405
Alekh Meka <alekhmeka@gmail.com>
2023-01-23 18:55:28 -0500

    Resolved RQ2 issue with mph-table

--- artifacts/experiments/RQ2/generateStats.py
+++ artifacts/experiments/RQ2/generateStats.py
@@ -4,8 +4,7 @@ import subprocess
 
 JAR_FILE = "target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar"
 
-#PROJECTS = ["convex", "jflex", "mph-table", "rpki-commons"]
-PROJECTS = ["rpki-commons"]
+PROJECTS = ["convex", "jflex", "mph-table", "rpki-commons"]
 
 TRIALS = [10, 50, 500, 1000]
 
