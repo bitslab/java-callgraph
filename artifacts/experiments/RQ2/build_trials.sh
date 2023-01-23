81ffa0c71d536673408512a7886805c7eacdd584
Alekh Meka <alekhmeka@gmail.com>
2023-01-23 18:45:51 -0500

    Resolved RQ2 issue with mph-table

--- artifacts/experiments/RQ2/build_trials.sh
+++ artifacts/experiments/RQ2/build_trials.sh
@@ -15,10 +15,10 @@ java -jar target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar git -c rpki-commo
 java -jar target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar git -c rpki-commons-500
 java -jar target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar git -c rpki-commons-1000
 
-java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/mph-table-10/mph-table-1.0.6-SNAPSHOT.jar -t ./artifacts/output/mph-table-10/mph-table-1.0.6-SNAPSHOT.jar -o mph-table-10_graph
-java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/mph-table-50/mph-table-1.0.6-SNAPSHOT.jar -t ./artifacts/output/mph-table-50/mph-table-1.0.6-SNAPSHOT.jar -o mph-table-50_graph
-java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/mph-table-500/mph-table-1.0.6-SNAPSHOT.jar -t ./artifacts/output/mph-table-500/mph-table-1.0.6-SNAPSHOT.jar -o mph-table-500_graph
-java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/mph-table-1000/mph-table-1.0.6-SNAPSHOT.jar -t ./artifacts/output/mph-table-1000/mph-table-1.0.6-SNAPSHOT.jar -o mph-table-1000_graph
+java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -c mph-table-10 -o mph-table-10_graph
+java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -c mph-table-50 -o mph-table-50_graph
+java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -c mph-table-500 -o mph-table-500_graph
+java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -c mph-table-1000 -o mph-table-1000_graph
 
 java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/convex-10/convex-core/convex-core-0.7.1-jar-with-dependencies.jar -t ./artifacts/output/convex-10/convex-core/convex-core-0.7.1-tests.jar -o convex-10_graph
 java -jar ./target/javacg-0.1-SNAPSHOT-jar-with-dependencies.jar build -j ./artifacts/output/convex-50/convex-core/convex-core-0.7.1-jar-with-dependencies.jar -t ./artifacts/output/convex-50/convex-core/convex-core-0.7.1-tests.jar -o convex-50_graph
